57b289359a87f2ff17e083f3f7b69a56f3ee8038
qinpengfei <qinpengfei.2008@163.com>
2018-10-30 23:22:21 +0800

    fix
    Signed-off-by: qinpengfei <qinpengfei.2008@163.com>

--- cmd/nginx/main.go
+++ cmd/nginx/main.go
@@ -1,7 +1,61 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"time"
+	"os"
+	"path/filepath"
+	"github.com/golang/glog"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"github.com/feifeiiiiiiiiiii/mini-ingress-nginx/internal/nginx"
+	"github.com/feifeiiiiiiiiiii/mini-ingress-nginx/internal/controller"
+	"github.com/feifeiiiiiiiiiii/mini-ingress-nginx/internal/handlers"
+)
 
 func main() {
-	log.Println("Hello Mini Ingress Nginx")
+	glog.Infof("Hello Mini Ingress Nginx")
+
+	var kubeconfig *string
+	var err error
+
+	home := os.Getenv("HOME")
+	if home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+	
+	kubeClient, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		glog.Fatalf("Failed to create client: %v.", err)
+	}
+
+	nginxBinaryPath := "/usr/sbin/nginx"
+	nginx.NewNginxController("/etc/nginx/", nginxBinaryPath, false)
+
+	lbcInput := controller.NewLoadBalancerControllerInput{
+		KubeClient:              kubeClient,
+		ResyncPeriod:            30 * time.Second,
+		Namespace:               "mini-ingress-nginx",
+		IngressClass:            "kubernetes.io/mini.nginx.ingress.class",
+	}
+
+	lbc := controller.NewLoadBalancerController(lbcInput)
+
+	// create handlers for resources we care about
+	ingressHandlers := handlers.CreateIngressHandlers(lbc)	
+	serviceHandlers := handlers.CreateServiceHandlers(lbc)
+	endpointHandlers := handlers.CreateEndpointHandlers(lbc)
+
+	lbc.AddResourceHandler("ingresses", ingressHandlers)
+	lbc.AddResourceHandler("services", serviceHandlers)
+	lbc.AddResourceHandler("endpoints", endpointHandlers)
+
+	glog.Infof("End Ingress Nginx")
 }
--- internal/nginx/nginx.go
+++ internal/nginx/nginx.go
@@ -1,1 +1,27 @@
 package nginx
+
+import (
+	"path"
+)
+
+// Controller updates NGINX configuration, starts and reloads NGINX
+type Controller struct {
+	nginxConfdPath        string
+	nginxSecretsPath      string
+	local                 bool
+	nginxBinaryPath       string
+	configVersion         int
+}
+
+// NewNginxController creates a NGINX controller
+func NewNginxController(nginxConfPath string, nginxBinaryPath string, local bool) *Controller {
+	ngxc := Controller{
+		nginxConfdPath:        path.Join(nginxConfPath, "conf.d"),
+		nginxSecretsPath:      path.Join(nginxConfPath, "secrets"),
+		local:                 local,
+		nginxBinaryPath:       nginxBinaryPath,
+		configVersion:         0,
+	}
+
+	return &ngxc
+}
