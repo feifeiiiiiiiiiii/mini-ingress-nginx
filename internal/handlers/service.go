d8ae08c904462c1841cefdc0c645de94e66d5681
qinpengfei <qinpengfei@jd.com>
2018-10-30 20:28:02 +0800

    add endpoint & service empty handler
    Signed-off-by: qinpengfei <qinpengfei@jd.com>

---
+++ internal/handlers/service.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+	"github.com/feifeiiiiiiiiiii/mini-ingress-nginx/internal/controller"
+	"k8s.io/client-go/tools/cache"
+)
+
+// CreateServiceHandlers builds the handler funcs for services
+func CreateServiceHandlers(lbc *controller.LoadBalancerController) cache.ResourceEventHandlerFuncs {
+	return cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+
+		},
+		DeleteFunc: func(obj interface{}) {
+
+		},
+		UpdateFunc: func(old, cur interface{}) {
+
+		},
+	}
+}
