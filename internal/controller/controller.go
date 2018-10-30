ccfe4138e4124cea3d513200eff07f27cbe407b1
qinpengfei <qinpengfei@jd.com>
2018-10-30 21:16:54 +0800

    fix
    Signed-off-by: qinpengfei <qinpengfei@jd.com>

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -3,7 +3,10 @@ package controller
 import (
 	"time"
 
+	"github.com/feifeiiiiiiiiiii/mini-ingress-nginx/internal/utils"
+	"github.com/golang/glog"
 	extensions "k8s.io/api/extensions/v1beta1"
+	"k8s.io/apimachinery/pkg/fields"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
@@ -16,11 +19,15 @@ const (
 // LoadBalancerController watches Kubernetes API and
 // reconfigures NGINX via NginxController when needed
 type LoadBalancerController struct {
-	client            kubernetes.Interface
-	ingressController cache.Controller
-	namespace         string
-	resync            time.Duration
-	ingressClass      string
+	client             kubernetes.Interface
+	ingressController  cache.Controller
+	namespace          string
+	resync             time.Duration
+	ingressClass       string
+	ingressLister      utils.StoreToIngressLister
+	svcLister          cache.Store
+	endpointLister     utils.StoreToEndpointLister
+	endpointController cache.Controller
 }
 
 // NewLoadBalancerControllerInput holds the input needed to call NewLoadBalancerController.
@@ -53,4 +60,28 @@ func (lbc *LoadBalancerController) IsNginxIngress(ing *extensions.Ingress) bool 
 		return class == lbc.ingressClass || class == ""
 	}
 	return false
+}
+
+// AddResourceHandler adds the handlers for ingress\services
+func (lbc *LoadBalancerController) AddResourceHandler(resource string, handlers cache.ResourceEventHandlerFuncs) {
+	store, controller := cache.NewInformer(
+		cache.NewListWatchFromClient(
+			lbc.client.Extensions().RESTClient(),
+			resource,
+			lbc.namespace,
+			fields.Everything()),
+		&extensions.Ingress{},
+		lbc.resync,
+		handlers,
+	)
+	switch resource {
+	case "ingresses":
+		lbc.ingressLister.Store = store
+		lbc.ingressController = controller
+	case "endpoints":
+		lbc.endpointLister.Store = store
+		lbc.endpointController = controller
+	default:
+		glog.Fatalf("unknown resource %v", resource)
+	}
 }
